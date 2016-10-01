b0dfbeea7cb180e15e24274ce2db39abbecc9898
John Reynolds <john.reynolds.irl@acm.org>
2016-10-01 12:43:52 +0100

    Links support, mocked data for now
    Change-Id: Iae8fe753a7053a7daddb69f86e463cb2d52f4f72

--- server/src/main/webapp/management/management.config.js
+++ server/src/main/webapp/management/management.config.js
@@ -47,5 +47,6 @@
         $scope.favIcon = "/public/images/favicon.ico";
 //        $scope.favIcon = "/tiatus/config/images/favicon.ico";
         $scope.eventTitle = "Tiatus";
+        $scope.links = [{title: 'home', href: '/'}, {title: 'race', href: '/race'}, {title: 'logout', href: '/rest/logout'}]
     });
 })();
