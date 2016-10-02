a0498eb0086968e211bdd9d675701c8108293312
John Reynolds <john.reynolds.irl@acm.org>
2016-10-02 18:39:21 +0100

    Create and remove race test
    Change-Id: Ia3f3814a43a930bbc293f4e1f4e9971a29d533d8

--- server/src/test/e2e/management/race/race_spec.js
+++ server/src/test/e2e/management/race/race_spec.js
@@ -18,4 +18,29 @@ describe('src.test.e2e.management.race.race_spec.js', function() {
         var rows = table.$$('tr');
         expect(rows.count()).toBe(2);
     });
+
+    it('should have create race', function() {
+        var table = element(by.id('races'));
+        expect(table.isPresent());
+        var rows = table.$$('tr');
+        expect(rows.count()).toBe(2);
+
+        element(by.model('ctrl.race.name')).sendKeys('Race 1');
+        element(by.model('ctrl.race.order')).sendKeys('1');
+        element(by.id('createRace')).click();
+
+        rows = table.$$('tr');
+        expect(rows.count()).toBe(3);
+    });
+
+    it('should have remove race', function() {
+        var table = element(by.id('races'));
+        var rows = table.$$('tr');
+        expect(rows.count()).toBe(3);
+
+        element(by.repeater('race in ctrl.races').row(0)).element(by.css('button.btn')).click();
+
+        rows = table.$$('tr');
+        expect(rows.count()).toBe(2);
+    });
 });
