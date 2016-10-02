afcb1508e76d22d745556a0d0dc4e05733ff5630
John Reynolds <john.reynolds.irl@acm.org>
2016-10-02 17:55:13 +0100

    Some cleanup
    Change-Id: I9811b2d13fcfb313b1c512c3bb1ec4e2c92ab09e

--- server/src/main/webapp/management/management.controller.js
+++ server/src/main/webapp/management/management.controller.js
@@ -5,7 +5,6 @@
     // create service for these
         $scope.titleImage = "/public/images/stopwatch.svg";
         $scope.favIcon = "/public/images/favicon.ico";
-//        $scope.favIcon = "/tiatus/config/images/favicon.ico";
         $scope.eventTitle = "Tiatus";
         $scope.links = [{title: 'home', href: '#/'}, {title: 'race', href: '#/race'}, {title: 'logout', href: '/rest/logout'}]
     });
