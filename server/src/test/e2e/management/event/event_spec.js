ef2101e8247f29e5c217ecd598d07efaf717b596
John Reynolds <john.reynolds.irl@acm.org>
2016-11-04 19:36:45 +0000

    Correct drag and drop handling.
    Change-Id: If772459eeaa66c54afd13f94c065233dad9405f9

--- server/src/test/e2e/management/event/event_spec.js
+++ server/src/test/e2e/management/event/event_spec.js
@@ -1,3 +1,5 @@
+var dragAndDrop = require('html-dnd').code;
+
 describe('src.test.e2e.management.event.event_spec.js', function() {
     it('should be to get to page', function() {
         browser.get('/management/management.html#/event');
@@ -24,7 +26,7 @@ describe('src.test.e2e.management.event.event_spec.js', function() {
         expect(rows.count()).toBe(0);
     });
 
-    it('should be add event to unassigned', function() {
+    it('should be able to add unassiged event and move to assigned', function() {
 
         var assignedEvents = element(by.id('assignedEvents'));
         var assignedRows = assignedEvents.$$('.assigned-event');
@@ -46,30 +48,46 @@ describe('src.test.e2e.management.event.event_spec.js', function() {
         var unassignedEvent = element.all(by.repeater('event in ctrlUnassigned.unassigned')).get(0);
 
         // all this is done as selenium webdirver does not handle html5 drag and drop
-        var eventNameElement = unassignedEvent.element(by.css('.event-name'));
         unassignedEvent.getAttribute('id').then(function(txt){
             var id = txt;
-            var eventName;
-            eventNameElement.getText().then(function(txt){
-                var s = txt.split(': ');
-                eventName = s[1];
-                var data = "ctrlAssigned.dropOnAssigned({}, 0, {'id': " + id + ", 'name': '" + eventName + "'})";
-                assignedEvents.evaluate(data);
+            var draggable = browser.findElement(by.id(id));
+            var droppable = browser.findElement(by.id('assignedEvents'));
+            browser.driver.executeScript(dragAndDrop, draggable, droppable);
+        });
+    });
 
-                browser.get('/management/management.html#/event');
-                return browser.driver.wait(function() {
-                    return browser.driver.getCurrentUrl().then(function(url) {
-                        return /event/.test(url);
-                    });
-                }, 5000);
+    it('should be able to add a second unassiged event and move to assigned', function() {
 
-                unassignedRows = unassignedEvents.$$('.unassigned-event');
-                expect(unassignedRows.count()).toBe(0);
-                assignedRows = assignedEvents.$$('.assigned-event');
-                expect(assignedRows.count()).toBe(1);
+        var assignedEvents = element(by.id('assignedEvents'));
+        var assignedRows = assignedEvents.$$('.assigned-event');
+        expect(assignedRows.count()).toBe(1);
+        var unassignedEvents = element(by.id('unassignedEvents'));
+        var unassignedRows = unassignedEvents.$$('.unassigned-event');
+
+        // expect(unassignedRows.count()).toBe(0);
+        element(by.model('ctrl.event.name')).sendKeys('Event 2');
+        element(by.id('createEvent')).click();
+
+        unassignedRows = unassignedEvents.$$('.unassigned-event');
+        expect(unassignedRows.count()).toBe(1);
+        assignedRows = assignedEvents.$$('.assigned-event');
+        expect(assignedRows.count()).toBe(1);
+
+        var unassigned = element.all(by.repeater('event in ctrlUnassigned.unassigned'));
+        expect(unassigned.count()).toBe(1);
+        var unassignedEvent = element.all(by.repeater('event in ctrlUnassigned.unassigned')).get(0);
+        var assignedEvent = element.all(by.repeater('raceEvent in ctrl.assignedToRace')).get(0);
+
+
+        // all this is done as selenium webdirver does not handle html5 drag and drop
+        unassignedEvent.getAttribute('id').then(function(txt){
+            var id = txt;
+            assignedEvent.getAttribute('id').then(function(assignedId){
+                var draggable = browser.findElement(by.id(id));
+                var droppable = browser.findElement(by.id(assignedId));
+                browser.driver.executeScript(dragAndDrop, draggable, droppable);
             });
         });
 
-//        browser.actions().dragAndDrop(unassignedEvent, assignedEvents).perform(); -- replace above when html5 drag and drop is supported
     });
 });
