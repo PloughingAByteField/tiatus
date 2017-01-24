dd1d7442abb399aca4ce4963f06b6b219b2ae0c3
John Reynolds <john.reynolds.irl@acm.org>
2017-01-24 15:35:02 +0000

    Remove hash routing option as it defaults to html5
    Change-Id: Id66176f4426884cc5aa31c9fe89547eadc33181d

--- server/src/main/typescript/timing/app.routes.ts
+++ server/src/main/typescript/timing/app.routes.ts
@@ -1,10 +1,13 @@
-import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core'
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LandingComponent } from './landing';
 import { NoContentComponent } from './no-content';
 import { TimingComponent } from './time_entry';
 
-export const ROUTES: Routes = [
+const ROUTES: Routes = [
   { path: '',      component: LandingComponent },
   { path: 'race/:raceId',  component: TimingComponent },
   { path: '**',    component: NoContentComponent }
 ];
+
+export const appRouting: ModuleWithProviders = RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules });
