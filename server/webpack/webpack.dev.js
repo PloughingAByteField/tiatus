1bfc7fea9b879458394b2e222eba3513a57d4ee4
John Reynolds <john.reynolds.irl@acm.org>
2017-02-01 18:32:44 +0000

    Have dev server compress the files and run over https so http/2 is used.
    Change-Id: I01693bb709b51a4a04eff706e045c9f76d167efb

--- server/webpack/webpack.dev.js
+++ server/webpack/webpack.dev.js
@@ -17,7 +17,7 @@ const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
  * Webpack Constants
  */
 const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = webpackMerge(commonConfig({env: ENV}).metadata, {
@@ -152,7 +152,10 @@ module.exports = function (options) {
       },
       proxy: {
         "/rest": "http://127.0.0.1:8080"
-      }
+      },
+      compress: true,
+      https: true
+
     },
 
     /*
