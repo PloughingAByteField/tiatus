fb94b22ca9284aa023bd0b51684a8018334c0b80
John Reynolds <john.reynolds.irl@acm.org>
2017-02-20 17:25:41 +0000

    Add back to position capablity
    Change-Id: I52332589795a99253f937948a858d757799814ec

--- server/src/main/typescript/components/sidebar/sidebar.module.ts
+++ server/src/main/typescript/components/sidebar/sidebar.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from 'ng2-translate';
 
 @NgModule({
-    imports: [ CommonModule, RouterModule ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        TranslateModule.forRoot()
+    ],
     declarations: [
         SidebarComponent
     ],
