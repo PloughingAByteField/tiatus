b27e319b40becb1a765905a676289f7fd5e58ed3
John Reynolds <john.reynolds.irl@acm.org>
2017-03-06 19:02:32 +0000

    Add form with validation
    Change-Id: Ibb86378d13633d35cf06a29ab7af54f1af973983

--- server/src/main/typescript/admin/races/races.component.ts
+++ server/src/main/typescript/admin/races/races.component.ts
@@ -9,6 +9,10 @@ import { AdminRacesService } from './races.service';
   templateUrl: './races.component.html'
 })
 export class RacesComponent implements OnInit {
+  public model: Race = new Race();
+  public existingName: boolean = false;
+  public existingNumber: boolean = false;
+  public invalidNumber: boolean = false;
 
   public races: Race[];
 
@@ -16,7 +20,38 @@ export class RacesComponent implements OnInit {
   public ngOnInit() {
     console.log('hello from races');
     this.racesService.getRaces()
-      .subscribe((races: Race[]) => this.races = races);
+      .subscribe((races: Race[]) => {
+        this.races = races;
+        this.model.raceOrder = this.races.length + 1;
+      });
   }
 
+  public addRace(model: Race): void {
+    console.log(model);
+  }
+
+  public checkName(model: Race): void {
+    this.existingName = false;
+    let existingName: Race = this.races.filter((race: Race) => race.name === model.name).shift();
+    if (existingName) {
+      this.existingName = true;
+    }
+  }
+
+  public checkNumber(model: Race): void {
+    this.invalidNumber = false;
+    if (model.raceOrder < 0 || model.raceOrder > this.races.length + 1) {
+      console.log('invalid number');
+      this.invalidNumber = true;
+      return;
+    }
+
+    this.existingNumber = false;
+    let existingOrder: Race = this.races
+      .filter((race: Race) => race.raceOrder === model.raceOrder).shift();
+    if (existingOrder) {
+      this.existingNumber = true;
+    }
+
+  }
 }
