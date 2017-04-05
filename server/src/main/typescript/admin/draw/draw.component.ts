9a72a250eb1ebc13264e8b69c4abf2bf0df829d2
John Reynolds <john.reynolds.irl@acm.org>
2017-04-05 13:55:58 +0100

    Complete drag and drop support for draw
    Change-Id: I9566980d3a7200723cfd123573d8930072ac6193

--- server/src/main/typescript/admin/draw/draw.component.ts
+++ server/src/main/typescript/admin/draw/draw.component.ts
@@ -90,15 +90,9 @@ export class DrawComponent implements OnInit, OnDestroy {
 
   public onDrop(event: DragEvent, droppedOnEntry: Entry): void {
     console.log('dropped');
-    console.log(droppedOnEntry);
-    console.log(this.draggedEntry);
-    if (droppedOnEntry === null) {
-      console.log('dropped on table header');
-    } else {
-      console.log('dropped on entry');
-    }
     event.preventDefault();
     event.stopPropagation();
+    this.updateEntries(this.draggedEntry, droppedOnEntry);
     this.draggedEntry = null;
   }
 
@@ -137,6 +131,153 @@ export class DrawComponent implements OnInit, OnDestroy {
       return clubs;
   }
 
+  private updateEntries(entryBeingDropped: Entry, droppedOnEntry: Entry): void {
+    console.log('update');
+    console.log(droppedOnEntry);
+    console.log(entryBeingDropped);
+    let indexOfDroppedOnEntry: number = this.entriesForRace.indexOf(droppedOnEntry);
+    let indexOfEntryBeingDropped: number = this.entriesForRace.indexOf(entryBeingDropped);
+    console.log(indexOfDroppedOnEntry);
+    console.log(indexOfEntryBeingDropped);
+    if (indexOfDroppedOnEntry === indexOfEntryBeingDropped) {
+      console.log('dropped on self');
+      return;
+    }
+
+    let entryRacePosition: number = 1;
+    let entryNumber: number = 1;
+    let updatedEntries: Entry[];
+    if (droppedOnEntry === null) {
+      updatedEntries = this.droppedOnHeader(entryBeingDropped);
+    } else {
+      updatedEntries = this.droppedOnEntry(entryBeingDropped, droppedOnEntry);
+    }
+
+    this.sortEntriesbyRacePosition(this.entriesForRace);
+    console.log(updatedEntries);
+    console.log(this.entriesForRace);
+    if (updatedEntries.length > 0) {
+      this.entriesService.updateEntries(updatedEntries);
+    }
+  }
+
+  private droppedOnHeader(entryBeingDropped: Entry): Entry[] {
+    console.log('dropped on table header');
+    let updatedEntries: Entry[] = new Array<Entry>();
+    let indexOfEntryBeingDropped: number = this.entriesForRace.indexOf(entryBeingDropped);
+    if (!entryBeingDropped.fixedNumber) {
+      entryBeingDropped.number = 1;
+    }
+    entryBeingDropped.raceOrder = 1;
+    updatedEntries.push(entryBeingDropped);
+    for (let i = 0; i < indexOfEntryBeingDropped; i++) {
+      let updatedEntry: Entry = this.entriesForRace[i];
+      if (!entryBeingDropped.fixedNumber) {
+        this.incrementEntryNumber(updatedEntry);
+      }
+      this.incrementEntryRacePosition(updatedEntry);
+      updatedEntries.push(updatedEntry);
+    }
+
+    return updatedEntries;
+  }
+
+  private droppedOnEntry(entryBeingDropped: Entry, droppedOnEntry: Entry): Entry[] {
+    console.log('dropped on entry');
+    let updatedEntries: Entry[] = new Array<Entry>();
+    let indexOfDroppedOnEntry: number = this.entriesForRace.indexOf(droppedOnEntry);
+    let indexOfEntryBeingDropped: number = this.entriesForRace.indexOf(entryBeingDropped);
+    if (indexOfDroppedOnEntry < indexOfEntryBeingDropped) {
+      this.updateDroppedEntryDetails(entryBeingDropped, indexOfDroppedOnEntry, true);
+      updatedEntries.push(entryBeingDropped);
+      for (let i = indexOfDroppedOnEntry + 1; i < indexOfEntryBeingDropped; i++) {
+        let updatedEntry: Entry = this.entriesForRace[i];
+        if (!entryBeingDropped.fixedNumber) {
+          this.incrementEntryNumber(updatedEntry);
+        }
+        this.incrementEntryRacePosition(updatedEntry);
+        updatedEntries.push(updatedEntry);
+      }
+    } else {
+      this.updateDroppedEntryDetails(entryBeingDropped, indexOfDroppedOnEntry, false);
+      updatedEntries.push(entryBeingDropped);
+      for (let i = indexOfEntryBeingDropped + 1; i < indexOfDroppedOnEntry + 1; i++) {
+        let updatedEntry: Entry = this.entriesForRace[i];
+        if (!entryBeingDropped.fixedNumber) {
+          this.decrementEntryNumber(updatedEntry);
+        }
+        this.decrementEntryRacePosition(updatedEntry);
+        updatedEntries.push(updatedEntry);
+      }
+    }
+
+    return updatedEntries;
+  }
+
+  private sortEntriesbyRacePosition(entries: Entry[]): Entry[] {
+     return entries.sort((a: Entry, b: Entry) => {
+          if (a.raceOrder < b.raceOrder) {
+              return -1;
+          } else if (a.raceOrder === b.raceOrder) {
+              return 0;
+          } else if (a.raceOrder > b.raceOrder) {
+              return 1;
+          }
+      });
+  }
+
+  private updateDroppedEntryDetails(
+    entry: Entry, indexOfDroppedOn: number, increment: boolean): void {
+    let droppedOnRacePosition: number = 0;
+    let droppedOnEntry: Entry = this.entriesForRace[indexOfDroppedOn];
+    if (increment) {
+      entry.raceOrder = droppedOnEntry.raceOrder + 1;
+    } else {
+      entry.raceOrder = droppedOnEntry.raceOrder;
+    }
+    if (entry.fixedNumber) {
+      console.log('fixed number');
+      return;
+    }
+
+    let entryNumber: number = 0;
+    for (let i = indexOfDroppedOn; i >= 0; i--) {
+      let prevEntry: Entry = this.entriesForRace[i];
+      if (!prevEntry.fixedNumber) {
+        if (increment) {
+          entryNumber = prevEntry.number + 1;
+        } else {
+          entryNumber = prevEntry.number;
+        }
+        break;
+      }
+    }
+    if (entryNumber !== 0) {
+      console.log(entryNumber);
+      entry.number = entryNumber;
+    }
+  }
+
+  private incrementEntryRacePosition(entry: Entry): void {
+    entry.raceOrder = entry.raceOrder + 1;
+  }
+
+  private decrementEntryRacePosition(entry: Entry): void {
+    entry.raceOrder = entry.raceOrder - 1;
+  }
+
+  private incrementEntryNumber(entry: Entry): void {
+    if (!entry.fixedNumber) {
+      entry.number = entry.number + 1;
+    }
+  }
+
+  private decrementEntryNumber(entry: Entry): void {
+    if (!entry.fixedNumber) {
+      entry.number = entry.number - 1;
+    }
+  }
+
   private getEventForId(eventId: number): Event {
       for (let event of this.events) {
           if (event.id === eventId) {
@@ -156,6 +297,8 @@ export class DrawComponent implements OnInit, OnDestroy {
   }
 
   private getEntriesForRace(race: Race): Entry[] {
-    return this.entries.filter((entry: Entry) => entry.race === race.id);
+    let entries: Entry[] = this.entries.filter((entry: Entry) => entry.race === race.id);
+    this.sortEntriesbyRacePosition(entries);
+    return entries;
   }
 }
