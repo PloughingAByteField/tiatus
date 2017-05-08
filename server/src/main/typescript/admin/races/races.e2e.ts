4050661bbf4bac3325ecbdeb5b091215c620c297
John Reynolds <john.reynolds.irl@acm.org>
2017-05-08 20:55:52 +0100

    Add a second race
    Change-Id: I3b5a65abdda9464f689e26b9adf90304f137a8dd

--- server/src/main/typescript/admin/races/races.e2e.ts
+++ server/src/main/typescript/admin/races/races.e2e.ts
@@ -34,4 +34,24 @@ describe('Races', () => {
     expect(element(by.id('name-0')).getAttribute('value')).toBe('Race 1');
     expect(element(by.id('order-0')).getText()).toBe('1');
   });
+
+  it('should create a second new race on sumbmit', () => {
+    expect(element(by.id('submit')).isPresent()).toBe(true);
+    expect(element(by.id('submit')).isEnabled()).toBe(false);
+
+    const table = element(by.id('races'));
+    expect(table.isPresent()).toBe(true);
+    const rows = table.$$('tr');
+    expect(rows.count()).toBe(1);
+
+    element(by.css('input[formcontrolname=name]')).sendKeys('Race 2');
+    expect(element(by.id('submit')).isEnabled()).toBe(true);
+
+    element(by.id('submit')).click();
+    browser.wait(ExpectedConditions.visibilityOf(element(by.id('name-1'))), 5000) ;
+    expect(element(by.id('submit')).isEnabled()).toBe(false);
+    expect(element(by.css('input[formcontrolname=raceOrder]')).getAttribute('value')).toBe('3');
+    expect(element(by.id('name-1')).getAttribute('value')).toBe('Race 2');
+    expect(element(by.id('order-1')).getText()).toBe('2');
+  });
 });
