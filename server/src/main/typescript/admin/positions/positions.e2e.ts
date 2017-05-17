839560fb9eab8c220d61fe205311c9ad5694aa7a
John Reynolds <john.reynolds.irl@acm.org>
2017-05-17 21:22:09 +0100

    Add a third position
    Change-Id: I86416dc2499ed6db12394589c26056683ce8ee26

--- server/src/main/typescript/admin/positions/positions.e2e.ts
+++ server/src/main/typescript/admin/positions/positions.e2e.ts
@@ -46,4 +46,22 @@ describe('Positions', () => {
     expect(element(by.id('submit')).isEnabled()).toBe(false);
     expect(element(by.id('name-1')).getAttribute('value')).toBe('Position 2');
   });
+
+  it('should create a third new position on sumbmit', () => {
+    expect(element(by.id('submit')).isPresent()).toBe(true);
+    expect(element(by.id('submit')).isEnabled()).toBe(false);
+
+    const table = element(by.id('positions'));
+    expect(table.isPresent()).toBe(true);
+    const rows = table.$$('tr');
+    expect(rows.count()).toBe(2);
+
+    element(by.css('input[formcontrolname=newName]')).sendKeys('Position 3');
+    expect(element(by.id('submit')).isEnabled()).toBe(true);
+
+    element(by.id('submit')).click();
+    browser.wait(ExpectedConditions.visibilityOf(element(by.id('name-2'))), 5000) ;
+    expect(element(by.id('submit')).isEnabled()).toBe(false);
+    expect(element(by.id('name-2')).getAttribute('value')).toBe('Position 3');
+  });
 });
