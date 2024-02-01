52e00ac42609928198579bc5d06352d38768f5d2
John Reynolds <john.reynolds.irl@acm.org>
2024-02-01 22:24:47 +0000

    cleanup

--- server/src/main/typescript/admin/users/roles-http.service.ts
+++ server/src/main/typescript/admin/users/roles-http.service.ts
@@ -9,7 +9,6 @@ import { Data } from '../../model/data.model';
 @Injectable()
 export class AdminRolesHttpService extends CachedHttpService {
 
-    private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     private endPoint: string = '/rest/users/roles';
 
     constructor(protected http: HttpClient) {
