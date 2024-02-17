76ff9f6e58fcfeadc6217f686fab78bb00bad3c2
John Reynolds <john.reynolds.irl@acm.org>
2024-02-17 15:11:01 +0000

    Access for config and logo

--- server/src/main/java/org/tiatus/server/filter/ApplicationSecurity.java
+++ server/src/main/java/org/tiatus/server/filter/ApplicationSecurity.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,7 +20,6 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.security.web.csrf.CsrfFilter ;
 import org.springframework.security.web.csrf.CsrfToken;
 
 import jakarta.servlet.DispatcherType;
@@ -44,6 +42,7 @@ public class ApplicationSecurity {
     http
         .authorizeHttpRequests((authorize) -> authorize
             .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.ERROR).permitAll()
+            .requestMatchers("/tiatus/**").permitAll()
             .requestMatchers("/rest/**").permitAll()
             .requestMatchers("/results/**").permitAll()
             .anyRequest().authenticated())
