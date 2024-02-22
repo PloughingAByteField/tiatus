fe252512befc8d1d871280529f99897c64338e3a
John Reynolds <john.reynolds.irl@acm.org>
2024-02-22 18:04:12 +0000

    Handle reloads of the page to bounce back to the application where the routing will take care of it

--- server/src/main/java/org/tiatus/server/filter/AppConfig.java
+++ server/src/main/java/org/tiatus/server/filter/AppConfig.java
@@ -14,10 +14,86 @@ import jakarta.servlet.Filter;
 public class AppConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/admin/").setViewName("forward:/admin/index.html");
-        registry.addViewController("/adjudicator/").setViewName("forward:/adjudicator/index.html");
-        registry.addViewController("/timing/").setViewName("forward:/timing/index.html");
-        registry.addViewController("/results/").setViewName("forward:/results/index.html");
+        //admin
+        registry
+            .addViewController("/admin")
+            .setViewName("forward:/admin/index.html");
+        registry
+            .addViewController("/admin/")
+            .setViewName("forward:/admin/index.html");
+        registry
+            .addViewController("/admin/{path2:[\\w]+}")
+            .setViewName("forward:/admin/index.html");
+        registry
+            .addViewController("/admin/{path2}/{path3:[\\w]+}")
+            .setViewName("forward:/admin/index.html");
+        registry
+            .addViewController("/admin/{path2}/{path3}/{path4:[\\w]+}")
+            .setViewName("forward:/admin/index.html");
+        registry
+            .addViewController("/admin/{path2}/{path3}/{path4}/{path5:[\\w]+}")
+            .setViewName("forward:/admin/index.html");
+
+        // adjudicator
+        registry
+            .addViewController("/adjudicator")
+            .setViewName("forward:/adjudicator/index.html");
+        registry
+            .addViewController("/adjudicator/")
+            .setViewName("forward:/adjudicator/index.html");
+        registry
+            .addViewController("/adjudicator/{path2:[\\w]+}")
+            .setViewName("forward:/adjudicator/index.html");
+        registry
+            .addViewController("/adjudicator/{path2}/{path3:[\\w]+}")
+            .setViewName("forward:/adjudicator/index.html");
+        registry
+            .addViewController("/adjudicator/{path2}/{path3}/{path4:[\\w]+}")
+            .setViewName("forward:/adjudicator/index.html");
+        registry
+            .addViewController("/adjudicator/{path2}/{path3}/{path4}/{path5:[\\w]+}")
+            .setViewName("forward:/adjudicator/index.html");
+        
+        // timing
+        registry
+            .addViewController("/timing")
+            .setViewName("forward:/timing/index.html");
+        registry
+            .addViewController("/timing/")
+            .setViewName("forward:/timing/index.html");
+        registry
+            .addViewController("/timing/{path2:[\\w]+}")
+            .setViewName("forward:/timing/index.html");
+        registry
+            .addViewController("/timing/{path2}/{path3:[\\w]+}")
+            .setViewName("forward:/timing/index.html");
+        registry
+            .addViewController("/timing/{path2}/{path3}/{path4:[\\w]+}")
+            .setViewName("forward:/timing/index.html");
+        registry
+            .addViewController("/timing/{path2}/{path3}/{path4}/{path5:[\\w]+}")
+            .setViewName("forward:/timing/index.html");
+                
+        // results
+        registry
+            .addViewController("/results")
+            .setViewName("forward:/results/index.html");
+        registry
+            .addViewController("/results/")
+            .setViewName("forward:/results/index.html");
+        registry
+            .addViewController("/results/{path2:[\\w]+}")
+            .setViewName("forward:/results/index.html");
+        registry
+            .addViewController("/results/{path2}/{path3:[\\w]+}")
+            .setViewName("forward:/results/index.html");
+        registry
+            .addViewController("/results/{path2}/{path3}/{path4:[\\w]+}")
+            .setViewName("forward:/results/index.html");
+        registry
+            .addViewController("/results/{path2}/{path3}/{path4}/{path5:[\\w]+}")
+            .setViewName("forward:/results/index.html");
+
     }
 
     @Bean
