72303ec85d3dfb5942e6de27daafe3542e773f6e
John Reynolds <john.reynolds.irl@acm.org>
2024-02-29 18:20:14 +0000

    Flyway is now in the spring boot app resources

--- flyway/src/main/resources/db/migration/v1/0_0_2/V1.0.2.10__Entry_Club_xref.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE entry_club_xref (
-    entry_id bigint NOT NULL REFERENCES entry (id),
-    club_id bigint NOT NULL REFERENCES club (id)
-);
-
-ALTER TABLE entry_club_xref ADD CONSTRAINT entry_club_xref_constraint UNIQUE (entry_id, club_id);
--- flyway/src/main/resources/db/migration/v1/0_0_2/V1.0.2.11__Entry_Position_Time.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE entry_position_time (
-    start_point boolean,
-    synced boolean,
-    "time" timestamp without time zone,
-    entry_id bigint NOT NULL REFERENCES entry (id),
-    position_id bigint NOT NULL REFERENCES "position" (id)
-);
-
-ALTER TABLE entry_position_time ADD CONSTRAINT entry_position_time_constraint UNIQUE (entry_id, position_id);
