96365e04772d8d8483adaeaf3522d76913436937
John Reynolds <john.reynolds.irl@acm.org>
2024-02-29 18:18:35 +0000

    Create races

--- server/src/test/e2e/admin/src/02_Races.ts
+++ server/src/test/e2e/admin/src/02_Races.ts
@@ -4,12 +4,40 @@ const races: NightwatchTests = {
 
   'Races': () => {
     browser
-    .url('http://127.0.0.1:8080/admin/')
-    .waitForElementVisible('body', 1000)
     .useXpath()
     .click("//a[text()='Race']")
     .pause(1000)
     .assert.textContains('//races', 'Add Race')
+  },
+
+  'Add Race 1': () => {
+    browser
+    .useCss()
+    .setValue('input[id=newName]', 'Race 1')
+    .setValue('input[id=newRaceOrder]', '1')
+    .click('button')  
+    .pause(5000)
+    .assert.elementsCount('races table tr', 1)
+  },
+
+  'Add Race 2': () => {
+    browser
+    .useCss()
+    .setValue('input[id=newName]', 'Race 2')
+    .setValue('input[id=newRaceOrder]', '2')
+    .click('button')  
+    .pause(1000)
+    .assert.elementsCount('races table tr', 2)
+  },
+
+  'Add Race 3': () => {
+    browser
+    .useCss()
+    .setValue('input[id=newName]', 'Race 3')
+    .setValue('input[id=newRaceOrder]', '3')
+    .click('button')  
+    .pause(1000)
+    .assert.elementsCount('races table tr', 3)
   }
 };
 
